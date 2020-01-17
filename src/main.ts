e1cf0302aced00b513e316edba8b5565caae55c4
SebastianSwboda <sebastian@swoboda.ch>
2020-01-17 13:29:02 +0100

    added Login

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import InputText from 'primevue/inputtext';
+import Password from 'primevue/password';
+
+import 'primevue/resources/themes/nova-light/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
 
 Vue.config.productionTip = false;
 
+Vue.component("InputText", InputText)
+Vue.component("Password", Password)
 new Vue({
   router,
   render: h => h(App)
---
+++ src/views/Login.vue
@@ -0,0 +1,25 @@
+<template>
+    <div style="align-content: center">
+        <span class="p-float-label">
+	<InputText id="username" type="text" v-model="username"/>
+	<label for="username">Username</label>
+</span>
+        <Password v-model="password"/>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Login",
+        data() {
+            return {
+                username: "",
+                password: ""
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
